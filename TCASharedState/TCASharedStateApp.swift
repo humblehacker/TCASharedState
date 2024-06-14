d1cb1109e7ed90585d11468af593d1f1e4670e4f
David Whetstone <david@humblehacker.com>
2024-06-14 14:35:04 -0700

    Initial Commit

---
+++ TCASharedState/TCASharedStateApp.swift
@@ -0,0 +1,17 @@
+//
+//  TCASharedStateApp.swift
+//  TCASharedState
+//
+//  Created by David Whetstone on 6/14/24.
+//
+
+import SwiftUI
+
+@main
+struct TCASharedStateApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
